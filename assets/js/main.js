d31e087cd1d6c38e14696597ab8bdcfdf651b421
matthew-woodington <smwoodington@gmail.com>
2022-09-18 15:27:25 -0400

    adds functioning game logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,26 @@ const newGameButton = document.querySelector(".new-game");
 
 gameDisplay.innerHTML = "Click New Game to Start!";
 
+var isTie = false;
 let tieCards1 = [];
 let tieCards2 = [];
 let tiePot = [];
+
+const cardValues = {
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  J: 11,
+  Q: 12,
+  K: 13,
+  A: 14,
+};
 
 const Player = function ({ name } = {}) {
   this.name = name;
@@ -61,13 +78,13 @@ Game.prototype.shuffle = function () {
 };
 
 Game.prototype.deal = function () {
-  // this.player1.hand;
-  // this.player2.hand;
-  // this.deck.cards;
-  for (let i = 0; i < this.deck.cards.length; i += 2) {
-    this.player1.hand.push(this.deck.cards[i]);
-    this.player2.hand.push(this.deck.cards[i + 1]);
-  }
+  this.player1.hand = this.deck.cards.slice(0, 50);
+  this.player2.hand = this.deck.cards.slice(50);
+
+  // for (let i = 0; i < this.deck.cards.length; i += 2) {
+  //   this.player1.hand.push(this.deck.cards[i]);
+  //   this.player2.hand.push(this.deck.cards[i + 1]);
+  // }
 };
 
 Game.prototype.flipCard = function () {
@@ -89,21 +106,21 @@ Game.prototype.updateDeckValue = function () {
 };
 
 Game.prototype.isWinner = function () {
-  const cardValues = {
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    10: 10,
-    J: 11,
-    Q: 12,
-    K: 13,
-    A: 14,
-  };
+  if (this.player1.hand.length === 0) {
+    gameDisplay.innerHTML = "Player2 Wins Game!";
+    player1DeckDisplay.innerHTML = "☹";
+    player1CardDisplay.innerHTML = "";
+    player2CardDisplay.innerHTML = "";
+    player2DeckDisplay.innerHTML = "☺";
+  } else if (this.player2.hand.length === 0) {
+    gameDisplay.innerHTML = "Player1 Wins Game!";
+    player1DeckDisplay.innerHTML = "☺";
+    player1CardDisplay.innerHTML = "";
+    player2CardDisplay.innerHTML = "";
+    player2DeckDisplay.innerHTML = "☹";
+  } else if (this.player1.hand.length > 0 || this.player1.hand.length > 0) {
+    return;
+  }
 
   if (
     cardValues[this.player1.activeCard.value] >
@@ -124,14 +141,93 @@ Game.prototype.isWinner = function () {
     cardValues[this.player2.activeCard.value]
   ) {
     gameDisplay.innerHTML = "It's a Tie!";
-    this.player1.hand.unshift(this.player1.activeCard);
-    this.player2.hand.unshift(this.player2.activeCard);
+    if (this.player1.hand.length <= 4 || this.player2.hand.length <= 4) {
+      this.player1.hand.unshift(this.player1.activeCard);
+      this.player2.hand.unshift(this.player2.activeCard);
+    } else {
+      isTie = true;
+      game.tieBreak();
+    }
   }
+  console.log(this.player1.hand, this.player2.hand);
 };
+
+Game.prototype.tieBreak = function () {
+  tieCards1 = this.player1.hand.splice(-3);
+  tieCards2 = this.player2.hand.splice(-3);
+  tiePot = tieCards1.concat(tieCards2);
+  tiePot.push(this.player1.activeCard, this.player2.activeCard);
+
+  alert(
+    `Tiebreaker Pot Active: last turn's cards as well as three extra cards each are on the line this next turn!`
+  );
+
+  console.log(tiePot);
+  isTie = true;
+};
+
+Game.prototype.addTieWinner = function () {
+  if (
+    cardValues[this.player1.activeCard.value] >
+    cardValues[this.player2.activeCard.value]
+  ) {
+    gameDisplay.innerHTML = "Player1 Wins Tiebreak!";
+    this.player1.hand.unshift(this.player1.activeCard);
+    this.player1.hand.unshift(this.player2.activeCard);
+    this.player1.hand = this.player1.hand.concat(tiePot);
+  } else if (
+    cardValues[this.player2.activeCard.value] >
+    cardValues[this.player1.activeCard.value]
+  ) {
+    gameDisplay.innerHTML = "Player2 Wins Tiebreak!";
+    this.player2.hand.unshift(this.player1.activeCard);
+    this.player2.hand.unshift(this.player2.activeCard);
+    this.player1.hand = this.player2.hand.concat(tiePot);
+  } else if (
+    cardValues[this.player1.activeCard.value] ===
+    cardValues[this.player2.activeCard.value]
+  ) {
+    gameDisplay.innerHTML = "It's a Tie!";
+    game.tieBreak();
+  }
+
+  console.log(this.player1.hand, this.player2.hand);
+
+  isTie = false;
+};
+
+// Game.prototype.checkWinner = function () {
+//   if (this.player1.hand.length > 0 || this.player1.hand.length > 0) {
+//     return;
+//   } else if (this.player1.hand.length === 0) {
+//     gameDisplay.innerHTML = "Player2 Wins Game!";
+//     player1DeckDisplay.innerHTML = "☹";
+//     player1CardDisplay.innerHTML = "";
+//     player2CardDisplay.innerHTML = "";
+//     player2DeckDisplay.innerHTML = "☺";
+//   } else if (this.player2.hand.length === 0) {
+//     gameDisplay.innerHTML = "Player1 Wins Game!";
+//     player1DeckDisplay.innerHTML = "☺";
+//     player1CardDisplay.innerHTML = "";
+//     player2CardDisplay.innerHTML = "";
+//     player2DeckDisplay.innerHTML = "☹";
+//   }
+// };
 
 Game.prototype.startGame = function () {
   game.shuffle();
   game.deal();
+};
+
+Game.prototype.reset = function () {
+  this.player1.hand = [];
+  this.player2.hand = [];
+
+  player1CardDisplay.innerHTML = "";
+  player2CardDisplay.innerHTML = "";
+
+  player1DeckDisplay.innerHTML = "";
+  player2DeckDisplay.innerHTML = "";
 };
 
 const game = new Game();
@@ -145,14 +241,23 @@ const game = new Game();
 console.log(game);
 
 newGameButton.addEventListener("click", () => {
+  game.reset();
+
   game.startGame();
 
   gameDisplay.innerHTML = "Draw Cards!";
 });
 
 drawButton.addEventListener("click", () => {
-  game.flipCard();
-  game.updateDisplay();
-  game.updateDeckValue();
-  game.isWinner();
+  if (isTie === false) {
+    game.flipCard();
+    game.updateDisplay();
+    game.updateDeckValue();
+    game.isWinner();
+  } else if (isTie !== false) {
+    game.flipCard();
+    game.addTieWinner();
+    game.updateDisplay();
+    game.updateDeckValue();
+  }
 });
