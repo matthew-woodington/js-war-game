0ab269702988378bf5f49b5b822d88d25516ba12
matthew-woodington <smwoodington@gmail.com>
2022-09-18 13:46:10 -0400

    adds refactor for main js including fix to constructor structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,3 @@
-const suits = ["♥", "♦", "♠", "♣"];
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
-
-const cardValues = {
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  10: 10,
-  J: 11,
-  Q: 12,
-  K: 13,
-  A: 14,
-};
-
-let inGame = false;
-let isTie = false;
-let tiePot = [],
-  tieCards1 = [],
-  tieCards2 = [];
-
-//// QUERY ////
-
 const player1CardDisplay = document.querySelector(".player1-card");
 const player2CardDisplay = document.querySelector(".player2-card");
 const player1DeckDisplay = document.querySelector(".player1-deck");
@@ -47,166 +6,153 @@ const gameDisplay = document.querySelector(".game-info");
 const drawButton = document.querySelector(".draw");
 const newGameButton = document.querySelector(".new-game");
 
-gameDisplay.innerHTML = "Draw to begin game!";
+gameDisplay.innerHTML = "Click New Game to Start!";
 
-//// CONSTRUCTORS ////
+let tieCards1 = [];
+let tieCards2 = [];
+let tiePot = [];
 
-function Deck(cards = newDeck()) {
-  this.cards = cards;
-}
-
-Deck.prototype.shuffle = function () {
-  this.cards.sort((a, b) => Math.random() - 0.5);
+const Player = function ({ name } = {}) {
+  this.name = name;
+  this.hand = [];
+  this.activeCard = {};
 };
 
-function newDeck() {
-  return suits.flatMap((suit) => {
-    return values.map((value) => {
-      return new Card(suit, value);
-    });
-  });
-}
-
-const mainDeck = new Deck();
-
-mainDeck.shuffle();
-console.log(mainDeck.cards);
-
-player1Deck = mainDeck.cards.slice(0, 26);
-player2Deck = mainDeck.cards.slice(26);
-
-// console.log(player1Deck);
-// console.log(player2Deck);
-
-///
-
-function Card(suit, value) {
+const Card = function ({ suit, value }) {
   this.suit = suit;
   this.value = value;
-}
+};
 
-///
-
-function Player(deck = [], card = {}) {
-  this.deck = deck;
-  this.card = card;
-}
-
-let player1 = new Player({
-  deck: player1Deck,
-});
-
-let player2 = new Player({
-  deck: player2Deck,
-});
-
-function Game(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
-}
-
-const game = new Game();
-
-Game.prototype.playGame = function () {
-  if ((inGame = false)) {
-    startGame();
+const Deck = function () {
+  this.cards = [];
+  const suits = ["♥", "♦", "♠", "♣"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      this.cards.push(new Card({ value: values[j], suit: suits[i] }));
+    }
   }
-  if ((inGame = true)) {
-    flipCard();
+  console.log(this.cards);
+};
+
+const Game = function () {
+  this.player1 = new Player({ name: "Player1" });
+  this.player2 = new Player({ name: "Player2" });
+
+  this.deck = new Deck();
+  console.log(this.deck);
+};
+Game.prototype.shuffle = function () {
+  this.deck.cards.sort((a, b) => Math.random() - 0.5);
+};
+
+Game.prototype.deal = function () {
+  // this.player1.hand;
+  // this.player2.hand;
+  // this.deck.cards;
+  for (let i = 0; i < this.deck.cards.length; i += 2) {
+    this.player1.hand.push(this.deck.cards[i]);
+    this.player2.hand.push(this.deck.cards[i + 1]);
   }
 };
 
-//// ---- ////
+Game.prototype.flipCard = function () {
+  this.player1.activeCard = this.player1.hand.pop();
+  this.player2.activeCard = this.player2.hand.pop();
 
-drawButton.addEventListener("click", () => {
-  game.playGame();
-});
+  console.log(this.player1.activeCard);
+  console.log(this.player2.activeCard);
+};
 
-function startGame() {
-  inGame = true;
+Game.prototype.updateDisplay = function () {
+  player1CardDisplay.innerHTML = `${this.player1.activeCard.value}${this.player1.activeCard.suit}`;
+  player2CardDisplay.innerHTML = `${this.player2.activeCard.value}${this.player2.activeCard.suit}`;
+};
 
-  flipCard();
-}
+Game.prototype.updateDeckValue = function () {
+  player1DeckDisplay.innerHTML = this.player1.hand.length;
+  player2DeckDisplay.innerHTML = this.player2.hand.length;
+};
 
-function flipCard() {
-  activePlayer1Card = player1Deck.pop();
-  activePlayer2Card = player2Deck.pop();
+Game.prototype.isWinner = function () {
+  const cardValues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    J: 11,
+    Q: 12,
+    K: 13,
+    A: 14,
+  };
 
-  console.log(activePlayer1Card);
-  console.log(activePlayer2Card);
-
-  player1CardDisplay.innerHTML = `${this.activePlayer1Card.value}${this.activePlayer1Card.suit}`;
-  player2CardDisplay.innerHTML = `${this.activePlayer2Card.value}${this.activePlayer2Card.suit}`;
-
-  updateCardCount();
-
-  // if ((isTie = true)) {
-  //   if (winsRound(activePlayer1Card, activePlayer2Card)) {
-  //     gameDisplay.innerHTML = "Player1 wins Tie!";
-  //     player1Deck.unshift(activePlayer1Card);
-  //     player1Deck.unshift(activePlayer2Card);
-  //     player1Deck.push(tiePot);
-  //   } else if (winsRound(activePlayer2Card, activePlayer1Card)) {
-  //     gameDisplay.innerHTML = "Player2 wins Tie!";
-  //     player2Deck.unshift(activePlayer1Card);
-  //     player2Deck.unshift(activePlayer2Card);
-  //     player2Deck.push(tiePot);
-  //   }
-  //   tie = false;
-  // }
-
-  if (winsRound(activePlayer1Card, activePlayer2Card)) {
-    gameDisplay.innerHTML = "Player1 wins!";
-    player1Deck.unshift(activePlayer1Card);
-    player1Deck.unshift(activePlayer2Card);
-  } else if (winsRound(activePlayer2Card, activePlayer1Card)) {
-    gameDisplay.innerHTML = "Player2 wins!";
-    player2Deck.unshift(activePlayer1Card);
-    player2Deck.unshift(activePlayer2Card);
-  } else {
+  if (
+    cardValues[this.player1.activeCard.value] >
+    cardValues[this.player2.activeCard.value]
+  ) {
+    gameDisplay.innerHTML = "Player1 Wins!";
+    this.player1.hand.unshift(this.player1.activeCard);
+    this.player1.hand.unshift(this.player2.activeCard);
+  } else if (
+    cardValues[this.player2.activeCard.value] >
+    cardValues[this.player1.activeCard.value]
+  ) {
+    gameDisplay.innerHTML = "Player2 Wins!";
+    this.player2.hand.unshift(this.player1.activeCard);
+    this.player2.hand.unshift(this.player2.activeCard);
+  } else if (
+    cardValues[this.player1.activeCard.value] ===
+    cardValues[this.player2.activeCard.value]
+  ) {
     gameDisplay.innerHTML = "It's a Tie!";
-    player1Deck.unshift(activePlayer1Card);
-    player2Deck.unshift(activePlayer2Card);
-    // alert("Three cards on the line!");
-    // isTie = true;
-
-    // tieCards1 = player1Deck.splice(-3);
-    // tieCards2 = player2Deck.splice(-3);
-
-    // tiePot.push(tieCards1, tieCards2);
+    this.player1.hand.unshift(this.player1.activeCard);
+    this.player2.hand.unshift(this.player2.activeCard);
   }
+};
 
-  if (isGameOver(player1Deck)) {
-    gameDisplay.innerHTML = "Game Over! Player2 Wins!";
-    player2DeckDisplay.innerHTML = "☺";
-    player1DeckDisplay.innerHTML = "☹";
-  } else if (isGameOver(player2Deck)) {
-    gameDisplay.innerHTML = "Game Over! Player1 Wins!";
-    player1DeckDisplay.innerHTML = "☺";
-    player2DeckDisplay.innerHTML = "☹";
-  }
-}
-//// ---- ////
+Game.prototype.startGame = function () {
+  game.shuffle();
+  game.deal();
+};
 
-// updates the display for each player
-function updateCardCount() {
-  player1DeckDisplay.innerHTML = player1Deck.length;
-  player2DeckDisplay.innerHTML = player2Deck.length;
-}
+const game = new Game();
+// game.shuffle();
+// game.deal();
+// game.flipCard();
+// game.updateDisplay();
+// game.updateDeckValue();
+// game.isWinner();
 
-updateCardCount();
-
-// determines round winner, true if player false if computer
-function winsRound(card1, card2) {
-  return cardValues[card1.value] > cardValues[card2.value];
-}
-
-function isGameOver(deck) {
-  return deck.length === 0;
-}
-
-//// New Game ////
+console.log(game);
 
 newGameButton.addEventListener("click", () => {
-  location.reload();
+  game.startGame();
+
+  gameDisplay.innerHTML = "Draw Cards!";
+});
+
+drawButton.addEventListener("click", () => {
+  game.flipCard();
+  game.updateDisplay();
+  game.updateDeckValue();
+  game.isWinner();
 });
